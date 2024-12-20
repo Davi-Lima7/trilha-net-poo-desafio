4007f7827b1fc6cabdcbfbcc807ba65c07cfe89b
Davi <davilimadealmeida60@gmail.com>
2024-12-20 12:25:58 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("543667", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("", "", "", 1);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
